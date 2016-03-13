646fd42808fe306bed64322e887198de57fd0f58
sudeepk <sudeepk@ccs.neu.edu>
2016-03-13 17:30:43 -0400

    Server side - 6: Client files moved into client folder; refactored

--- public/assignment/client/js/config.js
+++ public/assignment/client/js/config.js
@@ -11,31 +11,31 @@
     function configuration($routeProvider) {
         $routeProvider
             .when("/home", {
-                templateUrl: "views/home/home.view.html",
+                templateUrl: "client/views/home/home.view.html",
                 controller: "HomeController"
             })
             .when("/login", {
-                templateUrl: "views/users/login.view.html",
+                templateUrl: "client/views/users/login.view.html",
                 controller: "LoginController"
             })
             .when("/profile", {
-                templateUrl: "views/users/profile.view.html",
+                templateUrl: "client/views/users/profile.view.html",
                 controller: "ProfileController"
             })
             .when("/register", {
-                templateUrl: "views/users/register.view.html",
+                templateUrl: "client/views/users/register.view.html",
                 controller: "RegisterController"
             })
             .when("/admin", {
-                templateUrl: "views/admin/admin.view.html",
+                templateUrl: "client/views/admin/admin.view.html",
                 controller: "AdminController"
             })
             .when("/fields", {
-                templateUrl: "views/forms/fields.view.html",
+                templateUrl: "client/views/forms/fields.view.html",
                 controller: "FieldsController"
             })
             .when("/forms", {
-                templateUrl: "views/forms/forms.view.html",
+                templateUrl: "client/views/forms/forms.view.html",
                 controller: "FormsController"
             })
             .otherwise({
