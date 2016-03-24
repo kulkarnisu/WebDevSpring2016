1ebb21e8cf46ba8fb5271c8feef38f5987e8131f
sudeepk <sudeepk@ccs.neu.edu>
2016-03-24 17:01:17 -0400

    Project Prototype -5: Adde connection model and service

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by sudeep on 3/24/16.
  */
+
+module.exports = function(app, uuid) {
+
+    var connectionModel = require("./models/connection/connection.model.js")();
+    
+    var connectionService = require("./services/connection.service.server.js")(app, connectionModel, uuid);
+
+}
--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(cookieParser());
 
 var assignmentApp = require("./public/assignment/server/app.js")(app, uuid);
 
+var formApp = require("./public/project/server/app.js")(app, uuid);
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
