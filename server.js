8c5b5c1ab7bb11ec1cdf1ed3e89aa777d1e2fcb2
sudeepk <sudeepk@ccs.neu.edu>
2016-03-29 18:51:27 -0400

    Database 2: Refactored Server js for openshift mongodb connection

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ var mongoose      = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/form_app';
 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
